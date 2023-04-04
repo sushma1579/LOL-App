2a5a646f977f5f281e32aa348c807a706eeebce0
Sushma <sushmaduvvi57@gmail.com>
2023-04-04 10:19:55 -0500

    button fragment is diaplaying

--- app/src/main/java/com/example/l_o_l/MainActivity.kt
+++ app/src/main/java/com/example/l_o_l/MainActivity.kt
@@ -2,10 +2,9 @@ package com.example.l_o_l
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+        }
     }
-}
--- app/src/main/java/com/example/l_o_l/RateFragment.kt
+++ app/src/main/java/com/example/l_o_l/RateFragment.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-
 class RateFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -15,5 +14,4 @@ class RateFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_rate, container, false)
     }
-
 }
