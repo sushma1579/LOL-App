0dd76c25930e42d56026292d6952b8a342f65ef0
Sushma <sushmaduvvi57@gmail.com>
2023-04-03 22:52:40 -0500

    changes made in fragment

--- app/src/main/java/com/example/l_o_l/MainActivity.kt
+++ app/src/main/java/com/example/l_o_l/MainActivity.kt
@@ -3,9 +3,9 @@ package com.example.l_o_l
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 }
